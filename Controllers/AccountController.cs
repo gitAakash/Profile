6f5efe7877b002566369a3b0c766f9c2a07ab9be
Aakash <aakash.pawar@wwindia.com>
2015-02-27 16:40:30 +0530

    test

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -13,6 +13,7 @@ namespace aakashPawar.Controllers
     //[Authorize]
     public class AccountController : Controller
     {
+
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
 
